8130a7743a6e07217578c53b42718881c53eb1fa
luis210606 <luisdgb.13@gmail.com>
2025-03-28 15:48:16 +0100

    clase añadida Rectangulo

---
+++ src/git_github/Rectangulo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git_github;
+
+/**
+ *
+ * @author luigonbor
+ */
+public class Rectangulo {
+    float alto,ancho;
+
+    public Rectangulo(float alto, float ancho) {
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+
+ 
+    
+    
+}
