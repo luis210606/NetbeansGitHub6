6041ff2075b4c8cc68e958c528328738406a4193
luis210606 <luisdgb.13@gmail.com>
2025-04-02 15:27:15 +0200

    clase añadida

---
+++ src/practicagithub/PracticaGitHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicagithub;
+
+/**
+ *
+ * @author luigonbor
+ */
+public class PracticaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
